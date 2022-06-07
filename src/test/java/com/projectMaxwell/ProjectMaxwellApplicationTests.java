bf46cd14f70eda7b73d02b554282e0a9e2c4b4ca
Your Name <you@example.com>
2022-06-07 21:18:42 +0300

    Initial commit

---
+++ src/test/java/com/projectMaxwell/ProjectMaxwellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectMaxwell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMaxwellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

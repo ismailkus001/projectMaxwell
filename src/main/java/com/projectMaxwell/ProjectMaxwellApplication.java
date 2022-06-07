bc26c2f7f34ec5068e02f0f23100381c3b9d6644
Your Name <you@example.com>
2022-06-07 23:48:50 +0300

    first push

--- src/main/java/com/projectMaxwell/ProjectMaxwellApplication.java
+++ src/main/java/com/projectMaxwell/ProjectMaxwellApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @ComponentScan
+@PropertySource(value = {"classpath:application.properties"})
 public class ProjectMaxwellApplication {
 
 	public static void main(String[] args) {
